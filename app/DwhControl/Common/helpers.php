f98a822728341328056912047edce27ebe133964
matthenning <pub@matthias-henning.de>
2021-07-24 16:21:44 +0200

    Fixed getCallingFunctionName() log helper to log the class and method calling the log method instead of the log method itself

--- app/DwhControl/Common/helpers.php
+++ app/DwhControl/Common/helpers.php
@@ -8,7 +8,7 @@ function getCallingFunctionName(?int $object_id): string
 {
     $caller = debug_backtrace();
     if (count($caller) < 3) return '*invalid backtrace*';
-    $caller = $caller[2];
+    $caller = $caller[3];
 
     $class = isset($caller['class']) ? $caller['class'] : '*UNKNOWN*';
     $function = isset($caller['function']) ? $caller['function'] : '*unknown*';
